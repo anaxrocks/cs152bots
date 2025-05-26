2824e6ef51996d39baca43861e73d199b4f6d633
matt-wolff <mbwolff@stanford.edu>
2025-05-25 21:13:22 -0700

    added args to train.py.

--- classifier/train.py
+++ classifier/train.py
@@ -6,17 +6,13 @@ import torch.nn.functional as F
 from torch.optim import AdamW
 from tqdm import tqdm
 import os
+import argparse
 
-
-torch.manual_seed(42)
-NUM_EPOCHS = 5
-BATCH_SIZE = 16
 FOUNDATION_MODEL = "Qwen/Qwen2.5-0.5B"
 DATASET = "ucberkeley-dlab/measuring-hate-speech"
 MODELS_DIR = 'classifier/models'
 
 def iterate_over_dataloader(model, tokenizer, optimizer, dataloader, split):
-    
     total_loss = 0
 
     for batch in tqdm(dataloader):
@@ -39,7 +35,7 @@ def iterate_over_dataloader(model, tokenizer, optimizer, dataloader, split):
     avg_loss = total_loss / len(dataloader)
     return avg_loss
 
-def main():
+def main(args):
     if not os.path.isdir(MODELS_DIR):
         print(f'{MODELS_DIR} directory does not exist. Canceling training run.')
         return
@@ -53,21 +49,27 @@ def main():
         sliding_window=None,
         pad_token_id=tokenizer.pad_token_id
     )
-    optimizer = AdamW(model.parameters())
+    optimizer = AdamW(model.parameters(), lr=args.learning_rate)
 
-    for i in range(NUM_EPOCHS):
-        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    for i in range(args.epochs):
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
         train_loss = iterate_over_dataloader(model, tokenizer, optimizer, train_dataloader, 'train')
         print(f'Epoch {i}, Train loss: {train_loss}')
         with torch.no_grad():
-            val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
+            val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)
             val_loss = iterate_over_dataloader(model, tokenizer, optimizer, val_dataloader, 'val')
         print(f'Epoch {i}, Val loss: {val_loss}')
 
     torch.save(
         model.state_dict(),
-        f'classifier/models/trained_classifier.pt'
+        f'classifier/models/seed-{args.seed}_epochs-{args.epochs}_batch-{args.batch_size}_lr-{args.learning_rate}.pt'
     )
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', default=42, type=int)
+    parser.add_argument('--epochs', default=5, type=int)
+    parser.add_argument('--batch_size', default=16, type=int)
+    parser.add_argument('--learning_rate', default=1e-5, type=float)
+    args = parser.parse_args()
+    main(args)
