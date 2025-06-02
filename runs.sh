8b63644d4af39dc1e38989424c9bd7cf7ece0abc
matt-wolff <mbwolff@stanford.edu>
2025-06-02 00:37:56 -0700

    updated runs.sh.

--- runs.sh
+++ runs.sh
@@ -1,1 +1,4 @@
-python classifier/test.py --threshold 0.925 > test_metrics_0.925.txt;
+python classifier/train.py --batch_size 24 > 1e-5_run.txt;
+python classifier/train.py --batch_size 24 --learning_rate 1e-4 > 1e-4_run.txt;
+python classifier/train.py --batch_size 24 --learning_rate 2e-5 > 2e-5_run.txt;
+sudo shutdown -h now;
