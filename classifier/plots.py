77e80693c1856b3465df1edb617c7d00658441d7
EC2 Default User <ec2-user@ip-172-31-19-244.us-west-2.compute.internal>
2025-06-02 16:22:06 +0000

    new metrics.

---
+++ classifier/plots.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+e4_train_losses = []
+e4_val_losses = []
+e5_train_losses = []
+e5_val_losses = []
+two_e5_train_losses = []
+two_e5_val_losses = []
--- runs.sh
+++ runs.sh
@@ -1,4 +1,1 @@
-python classifier/train.py --batch_size 24 > 1e-5_run.txt;
-python classifier/train.py --batch_size 24 --learning_rate 1e-4 > 1e-4_run.txt;
-python classifier/train.py --batch_size 24 --learning_rate 2e-5 > 2e-5_run.txt;
-sudo shutdown -h now;
+python classifier/test.py --threshold 0.99 > test_metrics_0.99.txt;
